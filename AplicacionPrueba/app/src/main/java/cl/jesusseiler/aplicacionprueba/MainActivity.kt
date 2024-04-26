334f411a4a2ff9a2a32598700398ae9f0bbd77d3
Jesus Seiler <ichbinseiler@gmail.com>
2024-04-26 16:43:08 -0400

    Subiendo cambios de aplicacion

--- AplicacionPrueba/app/src/main/java/cl/jesusseiler/aplicacionprueba/MainActivity.kt
+++ AplicacionPrueba/app/src/main/java/cl/jesusseiler/aplicacionprueba/MainActivity.kt
@@ -2,43 +2,23 @@ package cl.jesusseiler.aplicacionprueba
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import cl.jesusseiler.aplicacionprueba.ui.theme.AplicacionPruebaTheme
 import com.google.firebase.Firebase
 import com.google.firebase.database.database
+import android.content.Intent
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        //Para comprobar que funciona la conexion con Realtime Database
         val database = Firebase.database
         val myRef = database.getReference("message")
-
         myRef.setValue("Hola, Grupo de Aiep!")
-        super.onCreate(savedInstanceState)
-        setContent {
-            AplicacionPruebaTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Jesus, Jaime, Mauricio, Alejandro")
-                }
-            }
-        }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hola $name!",
-        modifier = modifier
-    )
+        super.onCreate(savedInstanceState)
+        //Abrir la ventana de login al iniciar la aplicacion
+        setContentView(R.layout.activity_login)
+    }
 }
